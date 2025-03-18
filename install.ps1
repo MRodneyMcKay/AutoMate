d17492b05b57bc534b2538f5b9b3677dc9022c1c
MRodneyMcKay <raduska.zsolti@gmail.com>
2025-03-18 15:22:52 +0100

    Provide the implementation for weekly UNP print issue #9 issue #17

--- install.ps1
+++ install.ps1
@@ -20,3 +20,15 @@ $action = New-ScheduledTaskAction -Execute "C:\Program Files\PowerShell\7\pwsh.e
 
 # Register the scheduled task without a trigger
 Register-ScheduledTask -TaskName "ToggleTheme" -Principal $principal -Action $action -Description "Runs the ToggleTheme script manually or when triggered by other means."
+
+# Define the path to your PowerShell script
+$scriptPath = "$PSScriptRoot\Printing.Printing\weeklyUNPprint.ps1"
+
+# Define the action to run the PowerShell script
+$action = New-ScheduledTaskAction -Execute "C:\Program Files\PowerShell\7\pwsh.exe" -Argument "-WindowStyle hidden -ExecutionPolicy Bypass -File `"$scriptPath`""
+
+# Create a trigger to execute the task every Friday at 15:00 until 06/13
+$trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Friday -At 3:00PM -EndBoundary "2025-06-13T23:59:59"
+
+# Register the scheduled task without a trigger
+Register-ScheduledTask -TaskName "PrintWeeklyUNP"  -Trigger $trigger -Principal $principal -Action $action -Description "Runs the printweeklyUNP script, which saves and prints shhets for UNP."
