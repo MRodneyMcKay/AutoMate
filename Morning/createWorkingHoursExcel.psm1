8eee44c24410761ed8fb484e4ff86432573ef275
MRodneyMcKay <raduska.zsolti@gmail.com>
2025-03-20 14:36:28 +0100

    Refactoring committed issue #8

--- Morning/createWorkingHoursExcel.psm1
+++ Morning/createWorkingHoursExcel.psm1
@@ -3,7 +3,47 @@ Add-Type -AssemblyName PresentationFramework
 [System.Reflection.Assembly]::LoadFrom("C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Windows.Forms.dll")
 [System.Reflection.Assembly]::LoadFrom("C:\Windows\assembly\GAC_MSIL\Microsoft.Office.Interop.Excel\15.0.0.0__71e9bce111e9429c\Microsoft.Office.Interop.Excel.dll ") 
 
-function getHours {
+function Show-ErrorMessage {
+    param (
+        [string]$Message,
+        [string]$Title = "Hiba"
+    )
+    [System.Windows.Forms.MessageBox]::Show(
+        $(New-Object -TypeName System.Windows.Forms.Form -Property @{ TopMost = $true }),
+        $Message,
+        $Title,
+        [System.Windows.Forms.MessageBoxButtons]::OK,
+        [System.Windows.MessageBoxImage]::Error
+    ) | Out-Null
+}
+
+function Find-File {
+    param (
+        [string]$Pattern,
+        [string]$Pattern2 = "",
+        [string]$Directory = "C:\Users\Hirossport\Desktop\",
+        [bool]$PromptIfNotFound = $false,
+        [string]$PromptTitle = "Fájl kiválasztása"
+    )
+    $items = "" -eq $Pattern2 ? (Get-ChildItem $Directory | Where-Object { $_.Name.Normalize() -match $Pattern } | Sort-Object -Property Name -Descending | Select-Object -First 1 ) : (Get-ChildItem $Directory | Where-Object { $_.Name.Normalize() -match $Pattern } | Where-Object Name -match $Pattern2 | Sort-Object -Property Name -Descending | Select-Object -First 1)
+       
+    if ($items) {
+        return $items.FullName
+    } elseif ($PromptIfNotFound) {
+        Show-ErrorMessage -Message "Nem találom a következőt: $PromptTitle"
+
+        $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+        $OpenFileDialog.initialDirectory = $Directory
+        $OpenFileDialog.filter = "Minden Excel fájl|*.xls;*.xlsx;*.xlsm"
+        $OpenFileDialog.Title = $PromptTitle
+        $OpenFileDialog.ShowDialog() |  Out-Null
+        $items = Get-ChildItem $Directory | Where-Object Name -match $OpenFileDialog.SafeFileName
+        return $items.FullName
+    }
+    return ""
+}
+
+function get-Hours {
     param (
     [string]
     #String, mely az igényekből tartalmazza a műszak leírását
@@ -31,13 +71,13 @@ function getHours {
         return $shift
     }
     elseif ($definition -eq "12óra") {
-        getHours("08:30-20:30")
+        get-Hours("08:30-20:30")
     }
     elseif ($definition -eq "2+4é") {
-        getHours ('13:30-01:30')
+        get-Hours ('13:30-01:30')
     }
     elseif ([regex]::Matches($definition, '(?<=\().+?(?=\))' ).Success) {
-        getHours(([regex]::Matches($definition, '(?<=\().+?(?=\))' ).Value))
+        get-Hours(([regex]::Matches($definition, '(?<=\().+?(?=\))' ).Value))
     }
     else {
         throw "Rossz formátum"
@@ -54,7 +94,7 @@ function getHours {
     #>
 }
 
-function getIgenyekHelper {
+function get-IgenyekHelper {
     param (
         $Map,
         [pscustomobject]
@@ -69,7 +109,7 @@ function getIgenyekHelper {
         {   
             $role =  [string] $igeny.Worksheet.Cells($i, 1).MergeArea().Value2
             try {
-                $pair = getHours $igeny.Worksheet.Cells($i, 2).Value2
+                $pair = get-Hours $igeny.Worksheet.Cells($i, 2).Value2
             }
             catch {
                 $igeny.Workbook.close($false)
@@ -86,13 +126,13 @@ function getIgenyekHelper {
         $Map
         Write-Output "Valami nem jó, nem egyezik az elszámolásba beírt órák száma az igénnyel"         
         $igeny.Application.visible=$true
-        [System.Windows.Forms.MessageBox]::Show($(New-Object -TypeName System.Windows.Forms.Form -Property @{TopMost=$true}), "$total Valami nem jó, nem egyezik az elszámolásba beírt órák száma az igénnyel. `nKérlek nézd át!", "Hiba", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.MessageBoxImage]::Error)
+        Show-ErrorMessage -Message "$total Valami nem jó, nem egyezik az elszámolásba beírt órák száma az igénnyel. `nKérlek nézd át!"
         exit
     }
     $igeny.Workbook.close($false)
 }
 
-function getIgenyek {
+function get-Igenyek {
     param (
         $muszakok,
         [string]
@@ -102,13 +142,12 @@ function getIgenyek {
     )
     $excelHelper = New-Object -comobject Excel.Application
     $excelHelper.visible = $false
-
-    getIgenyekHelper  $muszakok @{
+    get-IgenyekHelper  $muszakok @{
         "Application" = $excelHelper
         "Workbook"    = $excelHelper.Workbooks.Open($UszomesterIgenyPath)
         "Worksheet"   = $excelHelper.Workbooks.Open($UszomesterIgenyPath).Sheets.Item(1)
     } | Out-Null
-    getIgenyekHelper $muszakok  @{
+    get-IgenyekHelper $muszakok  @{
         "Application" = $excelHelper
         "Workbook"    = $excelHelper.Workbooks.Open($FrontOfficeIgenyPath)
         "Worksheet"   = $excelHelper.Workbooks.Open($FrontOfficeIgenyPath).Sheets.Item(1)
@@ -116,13 +155,66 @@ function getIgenyek {
     [System.Runtime.Interopservices.Marshal]::ReleaseComObject($excelHelper)
 }
 
-function open-DiakElszamolas {
+function open-StudentWorkReportFurdo {
+    
+    param (
+        [boolean]  $fillCompletely = $false,
+        [datetime] $yesterday = (Get-Date).AddDays(-1)
+    )
+    $base='C:\Users\Hirossport\Hiros Sport Nonprofit Kft\Hiros-sport - Dokumentumok\Furdo\Recepcio\'
+    $honap = $yesterday.ToString('MMMM')
+    $elszamolas = "$($base)Diák elszámolás\"
+    $saveAsDir= "$($elszamolas)$($yesterday.ToString('yyyy.MM')) - $honap\"
+    if (-Not ([System.IO.Directory]::Exists($SaveAsDir)))
+    {
+        mkdir -p $SaveAsDir
+    }
+    $savaAsFurdo= "$($saveAsDir)$($yesterday.ToString('yyyy.MM.dd.')).xlsx"
+
+    $igeny = [ordered]@{}
+
+    $patternfu = '^F.*(' + [regex]::escape($honap) + '|' + (Get-Culture).DateTimeFormat.GetMonthName($yesterday.Month) +').*.*ürdő.*\.xlsx$'
+    $PathFU = Find-File -Pattern $PatternFU -Pattern2 "^((?!Front).)*$" -PromptIfNotFound $FillCompletely -PromptTitle "Fürdő úszómester igényei"
+
+    $patternfp =  '^F.*(' + [regex]::escape($honap) + '|' + (Get-Culture).DateTimeFormat.GetMonthName($yesterday.Month) +').*Fürdő.*(azd|ront).*xlsx$'
+    $PathFP = Find-File -Pattern $patternfp -PromptIfNotFound $FillCompletely -PromptTitle "Fürdő front office igényei"
+
+    if (-not $PathFU -or -not $PathFP) {
+        $Empty = @{ definition = ""; hours = "" }
+        $igeny = [ordered] @{"Úszómester" = @($empty;$empty)
+        "Csúszda" = @($empty)
+        "Gyógymedence`nfelügyelet" = @($empty;$empty)
+        "Öltöző szolgálat" = @($empty;$empty)
+        Gyermekmegőrző = @($empty)
+        "Pénztáros" = @($empty;$empty)
+        Recepció = @($empty)
+    }
+    } else {
+        $Igeny = [ordered]@{}
+        Get-Igenyek -Muszakok $Igeny -UszomesterIgenyPath $PathFU -FrontOfficeIgenyPath $PathFP
+    }
+    
+    
+    if (0 -ne $igeny.Count) {
+        $furdosablon = Create-Template $igeny $fillCompletely $("Napi diákmunka elszámolás " + $yesterday.ToString("yyyy. MMMM dd."))
+        if (-Not ([System.IO.File]::Exists($savaAsFurdo)))
+        {
+            $furdosablon.Workbook.SaveAs($savaAsFurdo,[Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, $true)
+        }
+        $furdosablon.Application.visible=$true
+        #Set-ItemProperty -Path HKCU:\Software\Script -Name YesterdaysWorkingHours -value $Today.Day
+    }
+}
+
+function open-StudentWorkReportStrand {
+    
     param (
         [boolean]
         #Teljesen legyen-e kitöltve az elszámolás
         $fillCompletely,
         [datetime] $yesterday = (Get-Date).AddDays(-1)
     )
+
     $base='C:\Users\Hirossport\Hiros Sport Nonprofit Kft\Hiros-sport - Dokumentumok\Furdo\Recepcio\'
         $honap = $yesterday.ToString('MMMM')
         $elszamolas = "$($base)Diák elszámolás\"
@@ -131,117 +223,40 @@ function open-DiakElszamolas {
         {
             mkdir -p $SaveAsDir
         }
-        $savaAsFurdo= "$($saveAsDir)$($yesterday.ToString('yyyy.MM.dd.')).xlsx"
+
+    $igeny = [ordered]@{}
+    $patternsu = '^F.*(' + [regex]::escape($honap) + '|' + [regex]::escape((Get-Culture).DateTimeFormat.GetMonthName($yesterday.Month)) +').*Strand.*\.xlsx$'
+    $PathSU = Find-File -Pattern $PatternSU  -Pattern2 "^((?!Front).)*$" -PromptIfNotFound $FillCompletely -PromptTitle "Strand úszómester igényei"
     
-        $igeny = [ordered]@{}
+    $patternsp = '^F.*(' + [regex]::escape($honap) + '|' + [regex]::escape((Get-Culture).DateTimeFormat.GetMonthName($yesterday.Month)) +').*Strand.*(azd|ront).*xlsx$'
+    $PathSP = Find-File -Pattern $PatternSP -PromptIfNotFound $FillCompletely -PromptTitle "Strand front office igényei"
     
-        $patternfu = '^F.*(' + [regex]::escape($honap) + '|' + (Get-Culture).DateTimeFormat.GetMonthName($yesterday.Month) +').*.*ürdő.*\.xlsx$'
-        $items = Get-ChildItem C:\Users\Hirossport\Desktop\ | Where-Object {$_.Name.Normalize() -match ($patternfu)}| Where-Object Name -match ("^((?!Gazd).)*$") | Where-Object Name -match ("^((?!Front).)*$") | Sort-Object -Property Name -Descending | Select-Object -First 1
-        $PathFU = ""
-        if ($items.Length -ne 0)
-        {
-            $PathFU =  $items.FullName
+    if (-not $PathSU -or -not $PathSP) {
+        $Empty = @{ definition = ""; hours = "" }
+        $Igeny = [ordered]@{
+            Úszómester = @($Empty; $Empty; $Empty)
+            Csúszda = @($Empty)
+            "Vízőr (pancsoló)" = @($Empty)
+            "Gyógymedence" = @($Empty)
+            "Öltöző női" = @($Empty)
+            "Öltöző férfi" = @($Empty)
+            Kisegítő = @($Empty)
+            Pénztáros = @($Empty)
         }
-        elseif ($fillCompletely) {
-            [System.Windows.Forms.MessageBox]::Show($(New-Object -TypeName System.Windows.Forms.Form -Property @{TopMost=$true}), "Nem találom a Fürdő úszómester igényeit", "Hiba", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.MessageBoxImage]::Error)
-            $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-            $OpenFileDialog.initialDirectory = "C:\Users\Hirossport\Desktop\"
-            $OpenFileDialog.filter = "Minden Excel fájl|*.xls;*.xlsx;*.xlsm"
-            $OpenFileDialog.Title = "Fürdő úszómester igényei"
-            $OpenFileDialog.ShowDialog() |  Out-Null
-            $items = Get-ChildItem C:\Users\Hirossport\Desktop\ | Where-Object Name -match $OpenFileDialog.SafeFileName
-            $PathFU =  $items.FullName
-        }
-        $patternfp =  '^F.*(' + [regex]::escape($honap) + '|' + (Get-Culture).DateTimeFormat.GetMonthName($yesterday.Month) +').*Fürdő.*(azd|ront).*xlsx$'
-        $items = Get-ChildItem C:\Users\Hirossport\Desktop\ | Where-Object {$_.Name.Normalize() -match ($patternfp)} | Sort-Object -Property Name -Descending | Select-Object -First 1
-        $PathFP = ""
-        if ($items.Length -ne 0)
-        {
-            $PathFP =  $items.FullName
-        }
-        elseif ($fillCompletely) {
-            [System.Windows.Forms.MessageBox]::Show($(New-Object -TypeName System.Windows.Forms.Form -Property @{TopMost=$true}), "Nem találom a Fürdő front office igényeit", "Hiba", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.MessageBoxImage]::Error)
-            $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-            $OpenFileDialog.initialDirectory = "C:\Users\Hirossport\Desktop\"
-            $OpenFileDialog.filter = "Minden Excel fájl|*.xls;*.xlsx;*.xlsm"
-            $OpenFileDialog.Title = "Fürdő front ofice igényei"
-            $OpenFileDialog.ShowDialog() |  Out-Null
-            $items = Get-ChildItem C:\Users\Hirossport\Desktop\ | Where-Object Name -match $OpenFileDialog.SafeFileName
-            $PathFP =  $items.FullName
-        }
-        if ("" -eq $PathFU -or  "" -eq $PathFP) {
-            $empty = @{definition = ""; hours = ""}
-            $igeny = [ordered] @{"Úszómester" = @($empty;$empty); "Csúszda" = @($empty); "Gyógymedence`nfelügyelet" = @($empty;$empty); "Öltöző szolgálat" = @($empty;$empty); Gyermekmegőrző = @($empty); "Pénztáros" = @($empty;$empty); Recepció = @($empty);}
-        }
-        else {
-            getIgenyek $igeny $PathFU $PathFP | Out-Null
-        }
-        
-        
-        if (0 -ne $igeny.Count) {
-            $furdosablon = Create-Template $igeny $fillCompletely $("Napi diákmunka elszámolás " + $yesterday.ToString("yyyy. MMMM dd."))
-            if (-Not ([System.IO.File]::Exists($savaAsFurdo)))
-            {
-                $furdosablon.Workbook.SaveAs($savaAsFurdo,[Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, $true)
-            }
-            $furdosablon.Application.visible=$true
-            #Set-ItemProperty -Path HKCU:\Software\Script -Name YesterdaysWorkingHours -value $Today.Day
-        }
-    
-    
-    <#
-        $igeny = [ordered]@{}
-        $patternsu = '^F.*(' + [regex]::escape($honap) + '|' + [regex]::escape((Get-Culture).DateTimeFormat.GetMonthName($yesterday.Month)) +').*Strand.*\.xlsx$'
-        $items = Get-ChildItem C:\Users\Hirossport\Desktop\ | Where-Object {$_.Name.Normalize() -match ($patternsu)} | Where-Object Name -match ("^((?!Gazd).)*$") | Where-Object Name -match ("^((?!Front).)*$") | Sort-Object -Property Name -Descending | Select-Object -First 1
-        $PathSU=""
-        if ($items.Length -ne 0)
-        {
-            $PathSU =  $items.FullName
-        }
-        elseif ($fillCompletely) {
-            [System.Windows.Forms.MessageBox]::Show($(New-Object -TypeName System.Windows.Forms.Form -Property @{TopMost=$true}), "Nem találom a Strand úszómester igényeit", "Hiba", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.MessageBoxImage]::Error)
-            $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-            $OpenFileDialog.initialDirectory = "C:\Users\Hirossport\Desktop\"
-            $OpenFileDialog.filter = "Minden Excel fájl|*.xls;*.xlsx;*.xlsm"
-            $OpenFileDialog.Title = "Strand úszómester igényei"
-            $OpenFileDialog.ShowDialog() |  Out-Null
-            $items = Get-ChildItem C:\Users\Hirossport\Desktop\ | Where-Object Name -match $OpenFileDialog.SafeFileName
-            $PathSU =  $items.FullName
-        }
-        $patternsp = '^F.*(' + [regex]::escape($honap) + '|' + [regex]::escape((Get-Culture).DateTimeFormat.GetMonthName($yesterday.Month)) +').*Strand.*(azd|ront).*xlsx$'
-        $items = Get-ChildItem C:\Users\Hirossport\Desktop\ | Where-Object {$_.Name.Normalize() -match ($patternsp)} | Sort-Object -Property Name -Descending | Select-Object -First 1
-        $PathSP = ""
-        if ($items.Length -ne 0)
-        {
-            $PathSP =  $items.FullName
-        }
-        elseif ($fillCompletely) {
-            [System.Windows.Forms.MessageBox]::Show($(New-Object -TypeName System.Windows.Forms.Form -Property @{TopMost=$true}), "Nem találom a Strand pénztár igényeit", "Hiba", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.MessageBoxImage]::Error)
-            $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-            $OpenFileDialog.initialDirectory = "C:\Users\Hirossport\Desktop\"
-            $OpenFileDialog.filter = "Minden Excel fájl|*.xls;*.xlsx;*.xlsm"
-            $OpenFileDialog.Title = "Strand front office igényei"
-            $OpenFileDialog.ShowDialog() |  Out-Null
-            $items = Get-ChildItem C:\Users\Hirossport\Desktop\ | Where-Object Name -match $OpenFileDialog.SafeFileName
-            $PathSP = $items.FullName
-        }    
-        $saveAsStrand= "$($saveAsDir)$($yesterday.ToString('yyyy.MM.dd.')) - STRAND.xlsx"  
-        if ("" -eq $PathFU -or "" -eq $PathFP) {
-            $empty = @{definition = ""; hours = ""}
-            $igeny = [ordered] @{Úszómester = @($empty;$empty;$empty); Csúszda = @($empty); "Vízőr (pancsoló)" = @($empty); "Gyógymedence" = @($empty); "Öltöző női" = @($empty); "Öltöző férfi" = @($empty); Kisegítő = @($empty); Pénztáros = @($empty)}
-        }
-        else {
-            getIgenyek $igeny $PathSU $PathSP | Out-Null
-        }
-        
-        if (0 -ne $igeny.Count) {
-            $strandsablon = Create-Template $igeny $fillCompletely $("Napi diákmunka elszámolás - STRAND - " + $yesterday.ToString("yyyy. MMMM dd."))
-            if (-Not ([System.IO.File]::Exists($saveAsStrand)))
-            {
-                $strandsablon.Workbook.SaveAs($saveAsStrand,[Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, $true)
-            }
-            $strandsablon.Application.visible = $true
-        }
-        Set-ItemProperty -Path HKCU:\Software\Script -Name YesterdaysWorkingHours -value $Today.Day
-    #>    
+    } else {
+        $Igeny = [ordered]@{}
+        Get-Igenyek -Muszakok $Igeny -UszomesterIgenyPath $PathSU -FrontOfficeIgenyPath $PathSP
     }
+
+    $saveAsStrand= "$($saveAsDir)$($yesterday.ToString('yyyy.MM.dd.')) - STRAND.xlsx"  
+    
+    if (0 -ne $igeny.Count) {
+        $strandsablon = Create-Template $igeny $fillCompletely $("Napi diákmunka elszámolás - STRAND - " + $yesterday.ToString("yyyy. MMMM dd."))
+        if (-Not ([System.IO.File]::Exists($saveAsStrand)))
+        {
+            $strandsablon.Workbook.SaveAs($saveAsStrand,[Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, $true)
+        }
+        $strandsablon.Application.visible = $true
+    }
+    #Set-ItemProperty -Path HKCU:\Software\Script -Name YesterdaysWorkingHours -value $Today.Day
+}
