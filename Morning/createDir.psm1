b15ad2c2ef2dd3f672479fb0ac7985fd74436fe7
MRodneyMcKay <raduska.zsolti@gmail.com>
2025-03-24 17:09:44 +0100

    Replace commands with native cmdlets issue #43

--- Morning/createDir.psm1
+++ Morning/createDir.psm1
@@ -1,23 +1,14 @@
 function create-Directories {
     param (
         [datetime]$Today = (Get-Date)
-    )
-    if ($Today.DayOfWeek.value__ -eq 0)
-    {
-        $Monday = $Today.AddDays(-6 - $Today.DayOfWeek.value__)
-        $Sunday = $Monday.AddDays(6)
-        $Week = $(get-date $Today -UFormat %V)
-    }
-    else {
-        $Monday = $Today.AddDays(1 - $Today.DayOfWeek.value__)
-        $Sunday = $Monday.AddDays(6)
-        $Week = $(get-date $Today -UFormat %V)
-    }
+    ) 
+    $Monday = $Today.AddDays($Today.DayOfWeek.value__ -eq 0 ? -6 : 1 - $Today.DayOfWeek.value__) 
+    $Sunday = $Monday.AddDays(6)
+    $Week = $(get-date $Today -UFormat %V)
     #HIBA mappa
     $base="C:\Users\Hirossport\Hiros Sport Nonprofit Kft\Hiros-sport - Dokumentumok\Furdo\Recepcio\HIBA\HIBA $($Today.Year)\"
     $dir = "$base$Week. hét ($($Monday.ToString("MM.dd.")) - $($Sunday.ToString("MM.dd.")))"
-    if (-Not ([System.IO.Directory]::Exists($dir)))
-    {
-        mkdir -p $dir
-    }   
+    if (-Not (Test-Path $dir)) {
+        New-Item -Path $dir -ItemType Directory -Force
+    }  
 }
