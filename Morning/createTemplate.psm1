b47c3b2bb29975b075d29a11802d3d07fbaaff3e
MRodneyMcKay <raduska.zsolti@gmail.com>
2025-03-28 20:29:04 +0100

    Proper licencing has been added to the project. issue #46

--- Morning/createTemplate.psm1
+++ Morning/createTemplate.psm1
@@ -1,3 +1,20 @@
+<#  
+    This file is part of sturdy-octo-garbanzo.  
+
+    sturdy-octo-garbanzo is free software: you can redistribute it and/or modify  
+    it under the terms of the GNU General Public License as published by  
+    the Free Software Foundation, either version 3 of the License, or  
+    (at your option) any later version.  
+
+    This program is distributed in the hope that it will be useful,  
+    but WITHOUT ANY WARRANTY; without even the implied warranty of  
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the  
+    GNU General Public License for more details.  
+
+    You should have received a copy of the GNU General Public License  
+    along with this program. If not, see <https://www.gnu.org/licenses/>.  
+#>
+
 [System.Reflection.Assembly]::LoadFrom("C:\Windows\assembly\GAC_MSIL\Microsoft.Office.Interop.Excel\15.0.0.0__71e9bce111e9429c\Microsoft.Office.Interop.Excel.dll ") 
 
 function RGB($r, $g, $b) { $r + ($g * 256) + ($b * 256 * 256) }
