21a97e06d23d0a86cd842b3e368068ca5edb83ae
MRodneyMcKay <raduska.zsolti@gmail.com>
2025-04-05 09:51:29 +0200

    implementing error handling in Morning routine, theming, popUpShiftManager,    maintenanceTicket, and RosterInformation
    issue #77

--- popUpShiftManager.ps1
+++ popUpShiftManager.ps1
@@ -19,12 +19,10 @@ Import-Module $PSScriptRoot\RosterInformation.psm1
 
 [System.Reflection.Assembly]::LoadFrom("C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Windows.Forms.dll")
 Add-Type -AssemblyName PresentationFramework
-
-$shiftManager =  (Get-ShiftManager)[(Get-Date).TimeOfDay -lt (New-TimeSpan -Hours 12 -Minutes 55) ? 0 : 1]
-
-if ($null -eq $shiftManager.Name) {
-    [System.Windows.Forms.MessageBox]::Show($(New-Object -TypeName System.Windows.Forms.Form -Property @{TopMost=$true}), "Nem tudom ki a $(($shiftManager.Shift -eq "1~*") ? "délelöttös" : "délutános") műszakvezető", "Vajon ki a müszi?", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.MessageBoxImage]::Error)
+try {
+    $shiftManager =  Get-ShiftManager
+} catch {
+    [System.Windows.Forms.MessageBox]::Show($(New-Object -TypeName System.Windows.Forms.Form -Property @{TopMost=$true}), "Nem tudom ki most a műszakvezető", "Vajon ki a müszi?", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.MessageBoxImage]::Error)
+    exit
 }
-else {
-    [System.Windows.Forms.MessageBox]::Show($(New-Object -TypeName System.Windows.Forms.Form -Property @{TopMost=$true}), "A $(($shiftManager.Shift -eq "1*") ? "délelöttös" : "délutános") műszakvezető: $($shiftManager.Name)", "Vajon ki a müszi?", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.MessageBoxImage]::Information)
-}
+[System.Windows.Forms.MessageBox]::Show($(New-Object -TypeName System.Windows.Forms.Form -Property @{TopMost=$true}), "A $((Get-Date).TimeOfDay  -lt (New-TimeSpan -Hours 12 -Minutes 55) ? "délelöttös" : "délutános") műszakvezető: $shiftManager", "Vajon ki a müszi?", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.MessageBoxImage]::Information)
