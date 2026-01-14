c1e516631281a8d1a39b196e57e3882d97129d19
MRodneyMcKay <raduska.zsolti@gmail.com>
2026-01-14 11:17:12 +0100

    Added reminders issue: #224

---
+++ Scripts/reminders.ps1
@@ -0,0 +1,59 @@
+param (
+    [string]$MessageID
+)
+<#  
+    This file is part of AutoMate.  
+
+    AutoMate is free software: you can redistribute it and/or modify  
+    it under the terms of the GNU General Public License as published by  
+    the Free Software Foundation, either version 3 of the License, or  
+    (at your option) any later version.  
+
+    This program is distributed in the hope that it will be useful,  
+    but WITHOUT ANY WARRANTY; without even the implied warranty of  
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the  
+    GNU General Public License for more details.  
+
+    You should have received a copy of the GNU General Public License  
+    along with this program. If not, see <https://www.gnu.org/licenses/>.  
+#>
+
+$ModulePath = Join-Path -Path $PSScriptRoot -ChildPath "..\Modules\"
+$resolvedModulegPath = (Resolve-Path -Path $ModulePath).Path
+Import-Module (Join-Path -Path $resolvedModulegPath -ChildPath 'LoggingSystem\LoggingSystem.psd1')
+[System.Reflection.Assembly]::LoadFrom("C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Windows.Forms.dll")
+Add-Type -AssemblyName PresentationFramework
+
+function Show-Message {
+    param (
+        [string]$Title,
+        [string]$Message
+    )
+    [System.Windows.Forms.MessageBox]::Show(
+        $(New-Object -TypeName System.Windows.Forms.Form -Property @{ TopMost = $true }),
+        $Message,
+        $Title,
+        [System.Windows.Forms.MessageBoxButtons]::OK,
+        [System.Windows.MessageBoxImage]::Information
+    ) | Out-Null
+}
+
+$messages = @{
+    "TIG" = @{
+        Title = "Teljesítés igazolás"
+        Text  = "Ellenőrzd a Teljesítés Igazolásokon a létszámokat!"
+    }
+    "Csúszda" = @{
+        Title = "Csúszda"
+        Text  = "A csúszda kinyitott"
+    }
+    "Szauna" = @{
+        Title = "Szauna"
+        Text  = "A szauna kinyitott"
+    }
+}
+
+if ($messages.ContainsKey($MessageID)) {
+    $msg = $messages[$MessageID]
+    Show-Message -Title $msg.Title -Message $msg.Text
+}
