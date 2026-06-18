65d64c32b2d9f3034b75c211e14a14ef60014d7b
MRodneyMcKay <raduska.zsolti@gmail.com>
2026-06-18 14:22:02 +0200

    Added Domb Beach report to the morning script. The report will be generated in the summer months (June, July, August, September) when the script is run.
    issue: #256

---
+++ Modules/StudentWorkReport/Public/BeachReport.ps1
@@ -0,0 +1,71 @@
+<#  
+    This file is part of AutoMate.  
+
+    AutoMate is free software: you can redistribute it and/or modify  
+    it under the terms of the GNU General Public License as published by  
+    the Free Software Foundation, either version 3 of the License, or  
+    (at your option) any later version.  
+
+    This program is distributed in the hope that it will be useful,  
+    but WITHOUT ANY WARRANTY; without even the implied warranty of  
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the  
+    GNU General Public License for more details.  
+
+    You should have received a copy of the GNU General Public License  
+    along with this program. If not, see <https://www.gnu.org/licenses/>.  
+#>
+
+function open-StudentWorkReportBeach {
+    
+    param (
+        [boolean]
+        #Teljesen legyen-e kitöltve az elszámolás
+        $fillCompletely,
+        [datetime] $yesterday = (Get-Date).AddDays(-1)
+    )
+
+    $base='C:\Users\Hirossport\Hiros Sport Nonprofit Kft\Hiros-sport - Dokumentumok\Furdo\Recepcio\'
+        $honap = $yesterday.ToString('MMMM')
+        $elszamolas = "$($base)Diák elszámolás\"
+        $saveAsDir= "$($elszamolas)$($yesterday.ToString('yyyy.MM')) - $honap\"
+        if (-Not (Test-Path $SaveAsDir)) {
+            New-Item -Path $SaveAsDir -ItemType Directory -Force
+        }
+
+    $igeny = [ordered]@{}
+    $PatternBU = '^F.*(' + [regex]::escape($honap) + '|' + [regex]::escape((Get-Culture).DateTimeFormat.GetMonthName($yesterday.Month)) +').*Beach.*\.xlsx$'
+    $PatternBp = '^F.*(' + [regex]::escape($honap) + '|' + [regex]::escape((Get-Culture).DateTimeFormat.GetMonthName($yesterday.Month)) +').*Beach.*(azd|ront).*xlsx$'
+    $saveAsBeach= "$($saveAsDir)$($yesterday.ToString('yyyy.MM.dd.')) - BEACH.xlsx" 
+    $Empty = @{ definition = ""; hours = "" }
+    $default = [ordered]@{
+        Úszómester = @($Empty)
+        Pénztáros = @($Empty)}
+    try {
+        $PathBU = Find-File -Pattern $PatternBU  -Pattern2 "^((?!Front).)*$" -PromptIfNotFound $FillCompletely -PromptTitle "Beach úszómester igényei"
+        $PathBP = Find-File -Pattern $PatternBp -PromptIfNotFound $FillCompletely -PromptTitle "Beach front office igényei"
+        Get-Igenyek -Muszakok $Igeny -UszomesterIgenyPath $PathBU -FrontOfficeIgenyPath $PathBP
+    } catch {        
+        $igeny = $default
+        Write-Log -Message $_.Exception.Message -Level "WARNING"
+        Write-Log -Message "Alapértelmezett igények használata" -Level "WARNING"
+    } finally {
+        if ($igeny.Count -ne 0) {
+            $beachsablon = Create-Template $igeny $fillCompletely $("Napi diákmunka elszámolás - Domb Beach - " + $yesterday.ToString("yyyy. MMMM dd."))
+            if (-Not (Test-Path $saveAsBeach)) {
+                $beachsablon.Workbook.SaveAs($saveAsBeach,[Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, $true)
+                Write-Log -Message "Az elszámolás elmentve: $saveAsBeach"
+            }
+            $beachsablon.Application.visible = $true
+        }
+        else {
+            Write-Log -Message "Nem volt igény a strand elszámolásra" -Level "INFO"
+            if ($beachsablon) {
+                $beachsablon.Workbook.Close($false)
+                $beachsablon.Application.Quit()
+                [System.Runtime.InteropServices.Marshal]::ReleaseComObject($beachsablon.Application) | Out-Null
+            }
+        }
+       
+    }
+    #Set-ItemProperty -Path HKCU:\Software\Script -Name YesterdaysWorkingHours -value $Today.Day
+}
